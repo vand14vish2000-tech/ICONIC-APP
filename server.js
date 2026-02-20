b223a80c9153a6420867145ee553a534e405fb37
vand14vish2000-tech <vand141220@gmail.com>
2026-02-21 02:10:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Page not found');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('✅ Server running at http://localhost:3000');
+});
